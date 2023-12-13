953b759292eb139c60b27993f3a7f7b0ecb0ced3
Oliviu Stoian <olivetty@gmail.com>
2023-12-13 14:03:42 +0200

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,26 @@
+# Integer variable
+integer_var = 10
+
+# Float variable
+float_var = 3.14
+
+# String variable
+string_var = "Hello, World!"
+
+# Boolean variable
+boolean_var = True
+
+# List variable
+list_var = [1, 2, 3, 4, 5]
+
+# Tuple variable
+tuple_var = (1, 2, 3, 4, 5)
+
+# Set variable
+set_var = {1, 2, 3, 4, 5}
+
+# Dictionary variable
+dict_var = {"name": "John", "age": 30}
+
+# None variable
+none_var = None

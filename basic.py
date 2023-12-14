406949a4f6ff2bf8d83864ac97813ad0f789b93e
Oliviu Stoian <olivetty@gmail.com>
2023-12-14 14:56:50 +0200

    .

--- basic.py
+++ basic.py
@@ -3,14 +3,22 @@
     This is a multi line comment
 """
 
+FILE_SIZE_LIMIT = 2000 # this is a constant
+
 # --- Integer variable
 integer_var = 10
+count = 10_000_000_000 # you can use the underscore (_) character to make numbers more readable
 
 # --- Float variable
-float_var = 3.14
+float_var = 3,14
 
 # --- String variable
 string_var = "Hello, World!"
+string_multiline = '''This is a multiline string
+We need to be very carefull about the indentation'''
+
+message = 'It\'s also a valid string' #To escape the quotes, you use the backslash (\) character
+greeting = 'Good ' 'Morning!' # When you place the string literals next to each other, Python automatically concatenates them into one string.
 
 # --- Boolean variable
 boolean_var = True
@@ -40,7 +48,18 @@ def my_function():
         print(f"{i} - My name is {name} and I am {age} years old.")         
         i += 1       
 
-my_function()
+my_function() # call the function
 
+str_val = '2'
+val = 0
+int(str_val) # convert a string to a integer number.
+float(str_val) # convert a string to a floating-point number.
+bool(val) # convert a value to a boolean value, either True or False.
+str(val) # return the string representation of a value.
+type(str_val) # return the type of a value.
+
+# --- print variables
 print(int(integer_var/2))
-print(str(float_var) + "\nis the py value")
+print(float_var)
+print(string_multiline)
+print(type(str_val))
--- if-statement.py
+++ if-statement.py
@@ -16,3 +16,22 @@ else:
 z = 5
 if z < 5 or z > 10:
     print("z is either less than 5 or greater than 10")
+
+# --- The following example illustrates how to use the backslash (\) character to continue a statement in the second line:
+a, b, c = True, False, True
+if (a == True) and (b == False) and \
+   (c == True):
+    print("Continuation of statements")
+
+# --- Example of price based by age
+age = input('Enter your age:') # get the age from the user
+your_age = int(age) # convert the string to int
+
+if your_age < 5: # determine the ticket price
+    ticket_price = 5
+elif your_age < 16:
+    ticket_price = 10
+else:
+    ticket_price = 18
+
+print(f'You\'ll pay ${ticket_price} for the ticket') # show the ticket price

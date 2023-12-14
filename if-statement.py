e5bf563bb074e30aae31e2e5ac69ddde07ee0eb4
Oliviu Stoian <olivetty@gmail.com>
2023-12-14 13:20:49 +0200

    Introduce comments and cleaned and verified the code.

--- if-statement.py
+++ if-statement.py
@@ -1,18 +1,23 @@
-# Example with a single condition
+""" This is a multi line comment
+    We need to be very carefull about the indentation
+    This is a multi line comment
+"""
+
+# --- Example with a single condition
 x = 10
 if x > 5:
     print("x is greater than 5")
 
-# Example with multiple conditions using 'and'
+# --- Example with multiple conditions using 'and'
 y = 7
-if 5 < y < 10:
+if 5 < y < 10 and y!= 7: # this is forced by i want to showcase the 'and' operator
     print("y is between 5 and 10")
-elif y > 10:
-    print("y is greater than 10")
+elif y > 10 or y < 5: # witch comes first true
+    print("y is greater or than 10 or smaller tham 5")
 else:
-    print("y is less than 5")
+    print("y is 7")
 
-# Example with multiple conditions using 'or'
+# --- Example with multiple conditions using 'or'
 z = 5
 if z < 5 or z > 10:
     print("z is either less than 5 or greater than 10")
